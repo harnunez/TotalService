6c7d405a748a249759e82089c14450d88c6c1dcb
harnefer_nunez <harnefer_nunez@itrsa.com.ar>
2019-11-28 17:16:17 -0300

    Se inicio las pantalla de Login y se modifico los archivos Manifest

--- app/src/main/java/com/example/totalservice/LoginActivity.java
+++ app/src/main/java/com/example/totalservice/LoginActivity.java
@@ -3,19 +3,15 @@ package com.example.totalservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         //Prueba de actualizacion
         //Test update
 
         //StartApp
-
-
-
-
     }
 }
