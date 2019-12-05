fd94244ed3e83cace8ce5c6137d67bac7384ae65
harnefer_nunez <harnefer_nunez@itrsa.com.ar>
2019-12-05 17:56:33 -0300

    Pantalla de Login finalizada. Se dio inicio a la creación de la pantalla para Crear Cuenta

--- app/src/main/java/com/example/totalservice/CreateAccountActivity.java
+++ app/src/main/java/com/example/totalservice/CreateAccountActivity.java
@@ -1,21 +1,14 @@
 package com.example.totalservice;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class CreateAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
-
-    }
-
-    public void goCreateAccount(View view) {
-
+        setContentView(R.layout.activity_create_account);
     }
 }
