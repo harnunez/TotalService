754b70cbe30e318b657098993afd9086f70b26e7
harnefer_nunez <harnefer_nunez@itrsa.com.ar>
2020-04-20 18:41:49 -0300

    Se agrego bottom navigation/ Fragments para tres views

---
+++ app/src/main/java/com/example/totalservice/ContainerActivity.java
@@ -0,0 +1,17 @@
+package com.example.totalservice;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+
+        //  BottomNavigationView btnNav = findViewById(R.id.navigation_bar);
+        // btnNav.setSelectedItemId(R.id.search);
+    }
+}
--- app/src/main/java/com/example/totalservice/LoginActivity.java
+++ app/src/main/java/com/example/totalservice/LoginActivity.java
@@ -3,10 +3,13 @@ package com.example.totalservice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity{
+
+   private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,12 @@ public class LoginActivity extends AppCompatActivity {
     public void goCreateAccount(View view) {
         Intent intent = new Intent(this,CreateAccountActivity.class);
         startActivity(intent);
-
     }
+
+
+   public void goToContainerActivity(View view){
+        Intent nIntent = new Intent(this, ContainerActivity.class);
+        startActivity(nIntent);
+   }
+
 }
